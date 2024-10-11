73b7b85139db2d33eb74c204914413cb48d9fa17
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-10-11 19:14:47 +0530

    React redux project 2 starting

---
+++ react-redux-2/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Router, Routes } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Router>
+          <Routes>
+            
+          </Routes>
+        </Router>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
